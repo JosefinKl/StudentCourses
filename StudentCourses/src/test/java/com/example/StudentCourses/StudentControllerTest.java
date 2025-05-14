654f5ba578fe9c25b21c409b74098031f37d4f90
josefinkl <josefin.klangefjall@gmail.com>
2025-05-14 10:58:40 +0200

    Created component test to create channel

---
+++ StudentCourses/src/test/java/com/example/StudentCourses/StudentControllerTest.java
@@ -0,0 +1,80 @@
+package com.example.StudentCourses;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+//Component test
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+class StudentControllerTest {
+
+    //Mocked Repo
+    StudentRepository studentRepository = mock(StudentRepository.class);
+    CourseRepository courseRepository = mock(CourseRepository.class);
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    //Component test to test create channel
+    @Test
+    void createStudent() throws Exception {
+        //Arrange
+        Student newStudent = new Student("Test", "TestLast");
+
+        //Act and Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/students")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newStudent)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", is("Test")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", is("TestLast")));
+
+    }
+
+}
