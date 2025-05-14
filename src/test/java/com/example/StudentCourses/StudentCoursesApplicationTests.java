745933d905f36878a23f252cb53316f71102cabc
josefinkl <josefin.klangefjall@gmail.com>
2025-05-14 20:58:02 +0200

    Deactivated unused test

--- src/test/java/com/example/StudentCourses/StudentCoursesApplicationTests.java
+++ src/test/java/com/example/StudentCourses/StudentCoursesApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.StudentCourses;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StudentCoursesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.StudentCourses;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class StudentCoursesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//}
