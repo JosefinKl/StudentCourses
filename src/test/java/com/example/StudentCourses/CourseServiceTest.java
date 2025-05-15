aab652ef21a9d852d859543c18aefd328cb23a8c
josefinkl <josefin.klangefjall@gmail.com>
2025-05-15 10:24:46 +0200

    Updated test ToAddNewCourse to have mocked result

--- src/test/java/com/example/StudentCourses/CourseServiceTest.java
+++ src/test/java/com/example/StudentCourses/CourseServiceTest.java
@@ -3,8 +3,11 @@ package com.example.StudentCourses;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
 class CourseServiceTest {
@@ -19,9 +22,12 @@ class CourseServiceTest {
         //Arrange
         Course course = new Course();
         course.setCourseName("Test");
+        when(mockedCourseRepo.save(course)).thenReturn(course);
         //Act
         Course c = courseService.addCourse(course);
         assertNotNull(c);
+        assertEquals(course.getCourseName(), c.getCourseName());
+
     }
 
     @Test
